f605714c062c9fe3fe7913e5d8254c5541ca5665
levdalba <levanidalbashvili00@gmail.com>
2023-10-25 00:48:38 +0700

    retrieve titles and length of animes

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import requests
 
+length = input("Do you want to see long or short anime? ").lower()
+
 for anime_id in range(1, 1000):
     url = f"https://api.jikan.moe/v4/anime/{anime_id}/full"
     response = requests.get(url)
 
     if response.status_code == 200:
-        anime_data = response.json()["data"]
+        anime_data = response.json()["data"].get("episodes")
 
-        print(anime_data["title"])
+        anime_title = response.json()["data"]["title"]
+        if length == "long" and anime_data is not None and anime_data >= 100:
+            print(f"{anime_title} has {anime_data} episodes")
+
+        elif length == "short" and anime_data is not None and anime_data < 100:
+            print(f"{anime_title} has {anime_data} episodes")
+        else:
+            continue
+
     else:
         continue
