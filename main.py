f7a172166b80adb9f9cbf7144d2b817a49081269
levdalba <levanidalbashvili00@gmail.com>
2023-10-29 05:31:52 +0700

    rewrited it using pandas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 length = input("Do you want to see long or short anime? ").lower()
 
@@ -8,15 +9,19 @@ for anime_id in range(1, 1000):
 
     if response.status_code == 200:
         anime_data = response.json()["data"].get("episodes")
+        anime_data = pd.Series(anime_data)
 
         anime_title = response.json()["data"]["title"]
-        if length == "long" and anime_data is not None and anime_data >= 100:
+        if length == "long" and anime_data is not None:
             print(f"{anime_title} has {anime_data} episodes")
 
-        elif length == "short" and anime_data is not None and anime_data < 100:
+        elif length == "short" and anime_data is not None:
             print(f"{anime_title} has {anime_data} episodes")
         else:
             continue
 
     else:
         continue
+
+
+myvar = pd.Series(a, index=["x", "y", "z"])
